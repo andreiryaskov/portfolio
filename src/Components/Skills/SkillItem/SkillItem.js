9f79323e5f07f7f083e1950a5e6b3c499e06e9bb
andreiryaskov <andreiryaskov@gmail.com>
2022-03-11 20:14:57 +1000

    icon

--- src/Components/Skills/SkillItem/SkillItem.js
+++ src/Components/Skills/SkillItem/SkillItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import style from './SkillItem.module.scss';
 import '../../../App.css';
+import img from '../../../img/react-brands.svg'
+
 
 const SkillItem = () => {
     return (
         <div className={style.skill_item}>
-            <div className={style.icon}></div>
-            <div className={style.descr}>React</div>
+            <img src='' alt=""/>
+            <h5 className={style.descr}>React</h5>
         </div>
     );
 };
