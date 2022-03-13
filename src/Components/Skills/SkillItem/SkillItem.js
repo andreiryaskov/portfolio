a4d3ee8a9ba06dd3203325d61fe5d1571f7806a1
andreiryaskov <andreiryaskov@gmail.com>
2022-03-13 22:15:38 +1000

    skills_media

--- src/Components/Skills/SkillItem/SkillItem.js
+++ src/Components/Skills/SkillItem/SkillItem.js
@@ -3,11 +3,15 @@ import style from './SkillItem.module.scss';
 import '../../../App.css';
 
 
-const SkillItem = () => {
+const SkillItem = (props) => {
     return (
         <div className={style.skill_item}>
-            <div className={style.icon}>icon</div>
-            <h5 className={style.descr}>React</h5>
+            <div className={style.icon}>{props.icon}</div>
+            <div className={style.description}>
+                <h4>{props.descr}</h4>
+                <p className={style.descr}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            </div>
+
         </div>
     );
 };
--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -2,23 +2,69 @@ import React from 'react';
 import style from './Skills.module.scss';
 import '../../App.css';
 import SkillItem from "./SkillItem/SkillItem";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faReact, faJs, faGitlab, faHtml5, faSass} from "@fortawesome/free-brands-svg-icons";
 
 const Skills = () => {
+
+    const skillsItemStyle = {
+        color: '#fff',
+        fontSize: '5rem'
+    }
+
+    const skillsItemIcon = {
+        react: {
+            icon: <FontAwesomeIcon style={skillsItemStyle}
+                                   icon={faReact} />,
+            data: 'React'
+        },
+        redux: {
+            icon: <FontAwesomeIcon  style={skillsItemStyle}
+                                    icon={faReact} />,
+            data: 'React-Redux'
+        },
+        ts: {
+            icon: <FontAwesomeIcon style={skillsItemStyle}
+                                   icon={faJs} />,
+            data: 'Type Script'
+        },
+        axios: {
+            icon: <FontAwesomeIcon style={skillsItemStyle}
+                                   icon={faGitlab} />,
+            data: 'Axios'
+        },
+        html: {
+            icon: <FontAwesomeIcon style={skillsItemStyle}
+                                   icon={faHtml5} />,
+            data: 'HTML, CSS'
+        },
+        sass: {
+            icon: <FontAwesomeIcon style={skillsItemStyle}
+                                   icon={faSass} />,
+            data: 'SASS'
+        }
+    }
+
     return (
         <div className='container'>
             <div className={style.skills}>
-                <h2 className={style.title}>What i do</h2>
+                <h2>What i do</h2>
                 <div className={style.wrapper}>
-                    <SkillItem/>
-                    <SkillItem/>
-                    <SkillItem/>
-                    <SkillItem/>
-                    <SkillItem/>
-                    <SkillItem/>
+                    <SkillItem icon={skillsItemIcon.react.icon}
+                               descr={skillsItemIcon.react.data}/>
+                    <SkillItem icon={skillsItemIcon.redux.icon}
+                               descr={skillsItemIcon.redux.data}/>
+                    <SkillItem icon={skillsItemIcon.ts.icon}
+                               descr={skillsItemIcon.ts.data}/>
+                    <SkillItem icon={skillsItemIcon.axios.icon}
+                               descr={skillsItemIcon.axios.data}/>
+                    <SkillItem icon={skillsItemIcon.html.icon}
+                               descr={skillsItemIcon.html.data}/>
+                    <SkillItem icon={skillsItemIcon.sass.icon}
+                               descr={skillsItemIcon.sass.data}/>
                 </div>
             </div>
         </div>
-
     );
 };
 
