23302baaeecce1dfef7a9d3fc392daac279fdb7c
andreiryaskov <andreiryaskov@gmail.com>
2022-03-23 11:01:53 +0300

    before_after_active

--- src/Components/Hamburger/Nav.js
+++ src/Components/Hamburger/Nav.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './Nav.module.scss';
 
 const Nav = ({isActive}) => {
 
+    const [burgerActive, setBurgerActive] = useState(false)
+
     const foo = () => {
         isActive()
+        setBurgerActive(!burgerActive)
     }
 
     return (
             <nav className={style.nav}>
-                <div className={style.burgerBtn}
+                <div className={burgerActive ? style.burgerBtn_active : style.burgerBtn}
                      onClick={foo}>
                     <span/>
                 </div>
